69f33e372825eca7029913528cae23c0d1886fb6
Bobsup10 <bobby.rossilli7@gmail.com>
2022-11-16 09:08:49 -0500

    Main Class

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,3 +1,12 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * Converts and calculates user's BMI based on height and weight
+ * @version 11/16/2022
+ * @author 24rossilli
+ */
 public class BMICalculator {
     /**
      * Convert English to metric units, perform the BMI calculation
@@ -7,6 +16,47 @@ public class BMICalculator {
      * @return user's BMI weight(kg)/height(m)^2 a double
      */
     public static double computeBMI(int inches, int pounds) {
+        double kgs, meters;
+        if (inches <= 0 || pounds <= 0)
+            return 0.;
+        meters = (inches * 0.0254);
+        kgs = (pounds * 0.454);
+        return (kgs / Math.pow(meters, 2));
+    }
+    /** Uses a Scanner to prompt the user for info, process the
+     * feet/inches conversion, calls the computeBMI method and prints the
+     * correct information.
+     * */
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+        String userin = new String();
+        int inches, pounds;
+        DecimalFormat df = new DecimalFormat("0.00");
+        while(true) {
+
+
+
+            // sanitize height and convert to inches
+
+            try {
+                System.out.print("Enter your height in feet and inches (Ex 6'1\") or 0 to quit: ");
+                userin = in.nextLine();
+                if(userin.equals("0"))
+                    break;
+
+                inches = Integer.parseInt(userin.substring(0, userin.indexOf("'"))) * 12;
+                inches += Integer.parseInt(userin.substring(userin.indexOf("'")+1, userin.length()-1));
+                // System.out.println("DEBUG: " + inches);
+                System.out.print("Enter your weight in pounds: ");
+                userin = in.nextLine();
+                pounds = Integer.parseInt(userin);
+                System.out.println("Your BMI, expressed as weight(kg)/height(m)^2: " + df.format(computeBMI(inches, pounds)) + " kg/m^2");
+            }
+            catch(Exception e)  {
+                System.out.println("There was an error, see here: " + e.toString());
+            }
+
+        }
 
     }
 }
